e3aa4efebad6ee45982ac19569ef69cf4b350fc5
hyroky867 <handm755@gmail.com>
2019-05-12 16:50:30 +0900

    コンテナコンポーネントを実装

---
+++ src/Presentation/Component/Container/TicTacToe.ts
@@ -0,0 +1,26 @@
+import { default as m } from 'mithril';
+import Panel from '../Presentational/Panel';
+import { default as ViewModel } from '../../ViewModel/TicTacToe';
+
+export default class TicTacToe implements m.ClassComponent {
+    private viewModel: ViewModel;
+
+    constructor(viewModel: ViewModel) {
+        this.viewModel = viewModel;
+    }
+
+    oninit() {
+        this.viewModel.setPanelList().then(() => {
+            m.redraw();
+        });
+    }
+
+    view() {
+        return m(
+            'div',
+            this.viewModel.getPanelList().map(row => {
+                return m(Panel, { entity: row, eventCreater: this.viewModel });
+            }),
+        );
+    }
+}
