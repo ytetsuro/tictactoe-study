531527daa6f0631bf29e02534bf3ab4ddc63db54
hyroky867 <handm755@gmail.com>
2019-05-12 16:51:32 +0900

    ビューにレンダリングする用のファイルを修正

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
-export default function () {
-	return 'a';
-}
+import { default as ViewModel } from './Presentation/ViewModel/TicTacToe';
+import GetPanelCollection from './Domain/UseCase/GetPanelCollection';
+import PanelListFetcher from './Data/Repository/PanelListFetcher';
+import { default as m } from 'mithril';
+import TicTacToe from './Presentation/Component/Container/TicTacToe';
+
+const panelListFetcher = new PanelListFetcher();
+const getPanelCollection = new GetPanelCollection(panelListFetcher);
+const viewModel = new ViewModel(getPanelCollection);
+
+m.mount(document.body, new TicTacToe(viewModel));
